ab0f6e7c8138b2a7d5d42117f6d16d8336e7c834
Ashish Pujari <ashish@amuratech.com>
2015-02-18 12:23:38 +0530

    Add Toast in Main activity

--- app/src/main/java/com/app/demo/MainActivity.java
+++ app/src/main/java/com/app/demo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +13,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 
+    public void init()
+    {
+        Toast.makeText(getApplicationContext(),"Demo",Toast.LENGTH_LONG).show();
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
